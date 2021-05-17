25276f0513a67c6dc0d3ecbb6bff7af75acca8b9
piotrgo00 <65816799+piotrgo00@users.noreply.github.com>
2021-05-17 22:13:01 +0200

    Dodaj pliki projektów.

---
+++ Utils/CustomFilterAttributes.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Filters.Utils
+{
+    public class CustomFilterAttributes : ResultFilterAttribute
+    {
+        public override async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+        {
+            var ipAddr = context.HttpContext.Connection.RemoteIpAddress.ToString();
+            var page = (PageResult)context.Result;
+            page.ViewData["filterMessage"] = ipAddr;
+            await next.Invoke();
+        }
+    }
+}
